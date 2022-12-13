41c6e7fd38824db8a428368761b2bc27e3a59bf0
요술토끼 <curious.wzrabbit@gmail.com>
2022-12-13 12:11:56 +0900

    feat(jnidriver): 최종 기능 구현

--- JNIDriver.java
+++ JNIDriver.java
@@ -6,11 +6,11 @@ public class JNIDriver implements JNIListener {
     private PushButtonThread pushButtonThread;
     final int SUCCESS = 1;
     final int FAIL = -1;
-   
+    
     static {
         System.loadLibrary("JNICDriver");
     }
-   
+    
     private native static int openDrivers();
     private native static void closeDrivers();
     private native static void writeLED(byte[] arr, int count);
@@ -24,85 +24,89 @@ public class JNIDriver implements JNIListener {
     public JNIDriver() {
         isConnected = false;
     }
-   
+    
     public int open() {
         if (isConnected) return FAIL;
         if (openDrivers() < 0) return FAIL;
-       
+        
         isConnected = true;
         pushButtonThread = new PushButtonThread();
         pushButtonThread.start();
-       
+        
         return SUCCESS;
     }
-   
+    
     public void displayLED(byte[] data) {
         if (!isConnected) return;
-       
+        
         writeLED(data, data.length);
     }
-   
+    
     public void setVibrator(int command) {
         if (!isConnected) return;
-       
+        
         writeVibrator((char) command);
     }
-   
+    
     public void playNote(int note) {
         if (!isConnected || note == -1) return;
-       
+        
         writePiezo((char) note);
     }
-   
+    
     public void displaySegment(byte[] data) {
 	    if (!isConnected) return;
 		
         writeSegment(data, data.length);
 	}
-   
+    
+    @Override
     public void onReceive(int value) {
         if (mainActivityTosser != null) {
             mainActivityTosser.onReceive(value);
+            
+            char testNote = '1';
+            writePiezo(testNote);
         }
     }
-   
+    
     public void setListener(JNIListener listener) {
         mainActivityTosser = listener;
     }
-   
+    
     public void setLCDText(String text, int slot) {
         if (!isConnected) return;
-       
+        
         writeLCDLine(text, text.length(), slot);
     }
-   
+    
     public void clearLCDText() {
         if (!isConnected) return;
-       
+        
         clearLCD();
     }
-   
+    
     public void close() {
         if (isConnected) {
             return;
         }
-       
+        
         closeDrivers();
     }
-   
+    
     public void finalize() throws Throwable {
         close();
         super.finalize();
     }
-   
-   
+    
+    
     private class PushButtonThread extends Thread {
         @Override
         public void run() {
             super.run();
-       
+        
             try {
-	            while (isConnected) {
+	            while (true) {
 	                try {
 	                    onReceive(getInterrupt());
 	                    Thread.sleep(100);
